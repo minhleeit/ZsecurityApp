078044ede9f4b826e227c3aafa53a64b394b41bd
Sindala <bsindala@uab.edu>
2021-10-13 23:34:03 -0500

    Initial app configuration.

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import PanicButton from './PanicButton';
+
+class App extends React.Component {
+  render() {
+    return (
+      <PanicButton/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/PanicButton.js
@@ -0,0 +1,87 @@
+import React from "react";
+import {
+    Button,
+    SafeAreaView, 
+    Text, 
+    View, 
+    StyleSheet, 
+    Alert,
+    TouchableOpacity
+} from 'react-native';
+
+const Seperator = () => (
+    <View style={styles.seperator}/>
+);
+
+class PanicButton extends React.Component {
+    render () {
+        return (
+            <SafeAreaView style={styles.container}>
+                <View>
+                    <Text style={styles.heading}>
+                        Welcome to ZSecurity App
+                    </Text>
+                </View>
+                <Seperator/>
+                <View style={styles.panicContainer}>
+                    <Text style={styles.title}>
+                        Are you in trouble?
+                    </Text>
+                    <TouchableOpacity 
+                        onPress={() => Alert.alert('Dialing: 911...')}
+                        style={styles.panicButton}>
+                        <Text style={styles.panicButtonTxt}>Press Me!!</Text>
+                    </TouchableOpacity>
+                </View>
+                <Seperator/>
+                <View>
+                    <Text style={styles.title}>
+                        Let's put the setup here...
+                    </Text>
+                </View>
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'center',
+        flex: 1,
+        marginHorizontal: 16,
+    },
+    heading: {
+        textAlign: 'center',
+        marginVertical: 8,
+        fontSize: 30,
+        marginTop: 0,
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 20,
+        marginVertical: 8,
+    },
+    seperator: {
+        marginVertical: 8,
+        borderBottomColor: '#737373',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    panicContainer: {
+        justifyContent: 'space-around',
+        backgroundColor: '#ddd',
+    },
+    panicButton: {
+        backgroundColor: '#ff0000',
+        width: 300,
+        marginHorizontal: 30,
+        marginVertical: 25,
+    },
+    panicButtonTxt: {
+        color: 'white',
+        textAlign: 'center',
+        marginVertical: 8,
+        fontSize: 20,
+    },
+})
+
+export default PanicButton;
